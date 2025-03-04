287628de4004768533d85d6af37ae28d18f803fe
Heath <duweihengmr@163.com>
2025-03-04 15:48:43 +0800

    init: :tada: 项目初始

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ],
+  clearScreen: false,
+  server: {
+    port: 5201,
+    strictPort: true,
+    host: '0.0.0.0',
+    hmr: true,
+    watch: {
+      ignored: ['**/src-tauri/**']
+    }
+  }
+}))
