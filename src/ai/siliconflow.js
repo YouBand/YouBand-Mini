60df3ed404f3026698ab62f1db1632ea193873bb
xihan123 <srxqzxs@vip.qq.com>
2025-04-08 18:39:06 +0800

    fix: 🎨 优化代码缩进

--- src/ai/siliconflow.js
+++ src/ai/siliconflow.js
@@ -1,9 +1,7 @@
 import AiBase from '@/ai/base.js'
 
-
-class SiliconFlowAI  {
+class SiliconFlowAI {
   static baseUrl = 'https://api.siliconflow.cn/v1/chat/completions'
-
 
   static async getResponseContent(roleCharacter, modelContent, context, message) {
     return await AiBase.openAI(SiliconFlowAI.baseUrl, roleCharacter, modelContent, context, message)
